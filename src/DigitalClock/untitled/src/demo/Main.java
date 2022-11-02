f7dc011927bce3172a76e0ff0b0c38d2471c96cd
RUBY <103845902+rubynarwal@users.noreply.github.com>
2022-11-02 23:39:55 +0530

    added

---
+++ src/DigitalClock/untitled/src/demo/Main.java
@@ -0,0 +1,19 @@
+package demo;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int x = 10;
+        int y = 20;
+        int sum = x + y;
+        int mul = x * y;
+        int div=x/y;
+        int mod=x%y;
+        System.out.println(sum);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(mod);
+
+    }}
