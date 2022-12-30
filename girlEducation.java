ac726f8e2c89f3770c9378763d00d9ae1a5f14a7
Ruby Narwal <103845902+rubynarwal@users.noreply.github.com>
2022-12-30 20:37:18 +0530

    Add files via upload

---
+++ girlEducation.java
@@ -0,0 +1,13 @@
+
+    import java.util.Scanner;
+
+    public class girlEducation {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your name:");
+            String name = scanner.nextLine();
+
+            System.out.println(String.format(", educating girls is important because it helps to break the cycle of poverty, improve health outcomes,  promote gender equality, and Moral Values will be there.", name));
+        }
+
+}
